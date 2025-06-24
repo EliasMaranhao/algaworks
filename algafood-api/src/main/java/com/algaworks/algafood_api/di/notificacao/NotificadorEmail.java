e9e5e69d4a42c87b64e8df4d085c0cc0f734450a
EliasMaranhao <elias.ma.antonio@gmail.com>
2025-06-24 08:15:17 -0300

    24062025

--- algafood-api/src/main/java/com/algaworks/algafood_api/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/di/notificacao/NotificadorEmail.java
@@ -6,9 +6,10 @@ import com.algaworks.algafood_api.di.modelo.Cliente;
 
 @Component
 public class NotificadorEmail implements Notificador{
-    
+
     @Override
     public void notificar(Cliente cliente, String mensagem){
-        System.out.printf("Notificando %s através do email %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.printf("Notificando %s através do email %s: usando %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
+
 }
